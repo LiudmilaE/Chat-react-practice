0d92d190425f843a4b011c3e9e5268e71cc062ae
Liudmyla <liudmyla.iefremova@gmail.com>
2018-06-05 13:05:59 +0300

    Init new project and create simple frontent side

---
+++ src/containers/MessagesList.js
@@ -0,0 +1,8 @@
+import { connect } from 'react-redux';
+import MessagesListComponent from '../components/MessagesList';
+
+const MessagesList = connect(state => ({
+    messages: state.messages
+}), {})(MessagesListComponent);
+
+export default MessagesList;
